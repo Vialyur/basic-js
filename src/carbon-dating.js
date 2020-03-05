67870493ef80d5411a593333e62d33903cf1c9e2
Vital Yurau <vialyur@list.ru>
2020-03-05 23:56:12 +0300

    feat: added solution of carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,7 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(sampleActivity) {
+  return (typeof sampleActivity !== "string"  || isNaN(parseFloat(sampleActivity)) || sampleActivity <= 0 || sampleActivity > 15) ? false :
+  Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) * HALF_LIFE_PERIOD / 0.693)
+}
